086f1626e5615c809bbab4b57a8895ace6fb19c8
Dvealiyah <alayh.smutairi@gmail.com>
2026-04-10 19:13:31 +0300

    fix cypress action

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,11 @@
+const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
 module.exports = defineConfig({
   e2e: {
-    video: true,   // 👈 هنا
-
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
     },
-
-    env: {
-      allure: true
-    }
-  }
+  },
 });
